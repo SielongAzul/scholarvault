ad4326afc56e5b76d48e60465260db89675b0ece
AzulSielong <azulsielong@gmail.com>
2024-03-16 02:25:18 +0800

    Added Registration

--- scholarship/models.py
+++ scholarship/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Scholarships(models.Model):
+    scholarship_name =  models.CharField(max_length=50)
+    scholarship_provider_name = models.CharField(max_length=50)
+    eligibility = models.TextField(null = True, blank = True)
+    requirements = models.TextField()
+    created = models.DateTimeField(auto_now_add = True)
+
+def __str__(self):
+            return self.scholarship_name
--- scholarship/views.py
+++ scholarship/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from .models import Scholarships
 
-# Create your views here.
+
+def home(request):
+
+    scholarships = Scholarships.objects.all()
+    context = {'scholarship':scholarships}
+    return (render) (request, 'scholarship/home.html', context)
+
+#context = {'name of context for views', what you declared in models.py}
+
+
+def account_login(request):
+    return (render) (request, 'scholarship/login_form.html')
